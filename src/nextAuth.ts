6bce1de84346866e426cfb2b37ad6e12b8db1efe
tains <tains2384@gmail.com>
2023-10-06 10:47:39 +0700

    chore: fix build

--- src/nextAuth.ts
+++ src/nextAuth.ts
@@ -1,6 +1,6 @@
 import { AuthOptions, NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { pick } from 'lodash';
+import pick from 'lodash/pick';
 
 const providers = [
   CredentialsProvider({
